353a3de15f669b2cbe75abfc97ed83f62a978fd4
messer82 <podinamarius82@gmail.com>
2025-10-27 23:54:43 +0200

    Project start

---
+++ src/main/java/com/app/BullyBeGone/BullyBeGoneApplication.java
@@ -0,0 +1,13 @@
+package com.app.BullyBeGone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullyBeGoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BullyBeGoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/BullyBeGone/BullyBeGoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.BullyBeGone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullyBeGoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
